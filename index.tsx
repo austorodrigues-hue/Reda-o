ec72966669b334c8868c32631715945806ec7317
austorodrigues-hue <austorodrigues@gmail.com>
2025-12-18 20:10:56 -0300

    Resolvendo

--- index.tsx
+++ index.tsx
@@ -1,16 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Este arquivo não é mais necessário, pois o aplicativo foi consolidado no index.html 
+// para permitir funcionamento direto no GitHub Pages sem processos de build.
